165173c447f67cc79e1557e7ce7addeac375a6ab
kbanchi1111 <kbanchi.1111@gmail.com>
2022-08-23 10:33:25 +0900

    open host port

--- app.py
+++ app.py
@@ -59,4 +59,5 @@ def get_random_string():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
